bfabfc7f2983544c0b3168b9d7de1bb9ebd7400b
Valérie <valleeriee@yahoo.fr>
2024-08-21 15:50:25 +0200

    Recherche sur la structure des fichiers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Fiche from './pages/fiche'
+import Apropos from './pages/apropos'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/fiche/" element={<Fiche />} />
+          <Route path="/apropos/" element={<Apropos />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
---
+++ src/pages/fiche/fiche.js
@@ -0,0 +1,9 @@
+function Fiche() {
+    return (
+        <div>
+            <h1>Fiche</h1>
+        </div>
+    )
+}
+
+export default Fiche
