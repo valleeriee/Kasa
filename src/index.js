df09b4f4f71c062cfd1d7bdf0adfb9e3f9080360
Valérie <valleeriee@yahoo.fr>
2024-08-21 17:26:11 +0200

    Structure pages et router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import App from './components/App'
 import Home from './pages/Home'
 import Fiche from './pages/Fiche'
 import Apropos from './pages/Apropos'
@@ -9,12 +10,13 @@ import Apropos from './pages/Apropos'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/fiche/" element={<Fiche />} />
-          <Route path="/apropos/" element={<Apropos />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/fiche/" element={<Fiche />} />
+            <Route path="/apropos/" element={<Apropos />} />
+        </Routes>
+      </Router>
+      <App />
   </React.StrictMode>
 );
