fc939d0423c9e3ae7ab38291161a2713ef766651
Valérie <valleeriee@yahoo.fr>
2024-08-29 00:42:18 +0200

    Début de la page Fiche logement

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -8,11 +8,19 @@ function Collapse(props) {
     const title = props.title ? <h2 className="tt-h2">{props.title}</h2> : ""
     const description = props.description ? <p className="paragraphe">{props.description}</p> : ""
 
+    const listeEquipements = props.equipments
+    const equipments = listeEquipements ? <ul className="paragraphe">
+            {listeEquipements.map((equipment, index) => (
+                <li key={`eqp-${index}`}>{equipment}</li>
+            ))}
+        </ul> : ""
+
     return (
         <div className="collapse" onClick={showDescription}>
             {title}
             <div className="clps-txt">
                 {description}
+                {equipments}
             </div>
         </div>
     );
--- src/pages/Fiche.js
+++ src/pages/Fiche.js
@@ -1,16 +1,36 @@
+import "../styles/fiche.scss"
 import { useParams } from 'react-router-dom'
 import data from '../datas/logements.json'
+import Collapse from "../components/Collapse"
 import Error from '../pages/Error'
 
 function Fiche() {
-    const logements = data.map(logement => logement.id);
-    
+    const logements = data.map(logement => logement.id)
+
     const {idLogement} = useParams()
+
+    const indexLogement = logements.indexOf(`${idLogement}`)
+    const titleLogement = data[indexLogement].title
+    const locationLogement = data[indexLogement].location
+    const tagsLogement = data[indexLogement].tags
+    const descriptionLogement = data[indexLogement].description
+    const equipmentsLogement = data[indexLogement].equipments
+    console.log(equipmentsLogement)
 
     return (
         logements.includes(`${idLogement}`) ? 
             <div className="page">
-                <h1>Fiche {idLogement}</h1>
+                <h1 className="tt-h1">{titleLogement}</h1>
+                <p className="paragraphe">{locationLogement}</p>
+                <ul className="tags">
+                    {tagsLogement.map((tag, index) => (
+                        <li key={`tag-${index}`}>{tag}</li>
+                    ))}
+                </ul>
+                <div className="details">
+                    <Collapse title="Description" description={descriptionLogement} />
+                    <Collapse title="Équipements" equipments={equipmentsLogement} />
+                </div>
             </div>
             : <Error />
     )
