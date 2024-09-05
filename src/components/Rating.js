f9668074ad1e232e1fb9ecd8525035e3897d5905
Valérie <valleeriee@yahoo.fr>
2024-09-05 16:01:15 +0200

    Modification du composant Rating et renommage du fichier home.scss

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -2,14 +2,17 @@ import "../styles/Rating.scss"
 
 function Rating(props) {
     const score = props.rating
-    const starsOn = [...Array(parseInt(score)).keys()]
-    const diff = 5 - score
-    const starsOff = [...Array(diff).keys()]
+    const stars = [...Array(5).keys()]
+
+    function buildStar(item) {
+        const isOn = item + 1 > score
+        const className = "star" + (isOn ? "" : " star-on")
+        return <li key={`on-${item}`}><span className={className}></span></li>
+    }
 
     return (
         <ul className="rating">
-            {starsOn.map(item => <li key={`on-${item}`}><span className="star star-on"></span></li>)}
-            {starsOff.map(item => <li key={`off-${item}`}><span className="star"></span></li>)}
+            {stars.map(buildStar)}
         </ul>
     )
   }
